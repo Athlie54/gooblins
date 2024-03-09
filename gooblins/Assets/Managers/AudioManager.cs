ba87cb246d774688bdded7e6134dc713422d9a8e
Athlie54 <kjself04@gmail.com>
2024-03-09 15:47:26 -0600

    AudioManager implemented
    This AudioManager SHOULD be able to be called in objects and play sounds as needed after they are listed and named in the Inspector

---
+++ gooblins/Assets/Managers/AudioManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+
+    // Start is called before the first frame update
+    // Awake, on the other hand, is called just before the Start() function would be
+    void Awake()
+    {
+        foreach(Sound s in sounds) {
+            //creates an audio source for every sound listed in the AudioManager inspector
+            s.source= gameObject.AddComponent<AudioSource>();
+            //finding the clip for each audio source
+            s.source.clip = s.clip;
+
+            // connecting the volume and pitch values for easier manipulation in the inspector
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    /* This function finds a sound with the same name as the name entered into the function parameter.
+     * It then plays the sound (if a sound with the same name is found).
+     */
+    public void Play (string name)
+    {
+        //Finding the sound such that the sound's name is equal to the given name
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
