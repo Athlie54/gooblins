51d00dead727cd03117253f01f551ced4a704410
WesGale05 <wesjgale05@gmail.com>
2024-03-10 04:43:26 -0500

    load art grid into scene

--- gooblins/Assets/Managers/GridManager.cs
+++ gooblins/Assets/Managers/GridManager.cs
@@ -1,7 +1,9 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,41 +11,86 @@ public class GridManager : MonoBehaviour {
     public static GridManager Instance;
     public static int _width, _height;
 
-    [SerializeField] private Tile _grassTile, _mountainTile;
+    [SerializeField] private Tile _grass, _mountain, _floor, _wall, _crack, _doorUp, _doorDown, _doorLeft, _doorRight;
 
     [SerializeField] private Transform _cam;
 
     public static Dictionary<Vector2, Tile> _tiles;
 
+    public TextAsset level;
+
+    public Tile tileToSpawn;
+
+
     void Awake() {
-        _width = 10;
-        _height = 10;
+        _width = 28;
+        _height = 28;
         Instance = this;
     }
 
     public void GenerateGrid()
     {
         _tiles = new Dictionary<Vector2, Tile>();
-        for (int x = 0; x < _width; x++)
+
+        List<int> tiles = new List<int>();
+
+
+
+        string[] levelContent = (level.ToString().Split(','));
+        foreach (string item in levelContent)
         {
-            for (int y = 0; y < _height; y++) {
-                var randomTile = Random.Range(0, 6) == 3 ? _mountainTile : _grassTile;
-                // Border around outside
-                if (y == 0 || y == _height - 1 || x == 0 || x == _width - 1) {
-                    randomTile = _mountainTile;
-                }
-                var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
+            int tile = int.Parse(item);
+            Debug.Log(tile);
+            tiles.Add(tile);
+        }
+       for (int y = 0; y < _width; y++)
+       {
+            for (int x = 0; x < _height; x++)
+            {
+                
+                switch (tiles[((_height - 1 - y) * _width) + x])
+                {
+                    case (int)TileTypes.Grass:
+                        tileToSpawn = _grass;
+                        break;
+                    case (int)TileTypes.Floor:
+                        tileToSpawn = _floor;
+                        break;
+                    case (int)TileTypes.Wall:
+                        tileToSpawn = _wall;
+                        break;
+                    case (int)TileTypes.Crack:
+                        tileToSpawn = _crack;
+                        break;
+                    case (int)TileTypes.DoorUp:
+                        tileToSpawn = _doorUp;
+                        break;
+                    case (int)TileTypes.DoorDown:
+                        tileToSpawn = _doorDown;
+                        break;
+                    case (int)TileTypes.DoorLeft:
+                        tileToSpawn = _doorLeft;
+                        break;
+                    case (int)TileTypes.DoorRight:
+                        tileToSpawn = _doorRight;
+                        break;
+                    default:
+                        Debug.Log("switch case failed");
+                        tileToSpawn = _mountain;
+                        break;
+                }  
+                
+                var spawnedTile = Instantiate(tileToSpawn, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
-              
-                spawnedTile.Init(x,y);
+
+                spawnedTile.Init(x, y);
                 spawnedTile._position = new Vector2Int(x, y);
 
                 Debug.Log($"Generated {spawnedTile._position}");
 
                 _tiles[new Vector2(x, y)] = spawnedTile;
             }
-        }
-
+       }
         _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
 
         GameManager.Instance.ChangeState(GameState.SpawnHeroes);
@@ -63,4 +110,18 @@ public class GridManager : MonoBehaviour {
         if (_tiles.TryGetValue(pos, out var tile)) return tile;
         return null;
     }
+
+}
+
+public enum TileTypes
+{
+    Air = 0,
+    Grass = 1,
+    Floor = 2,
+    Wall = 3,
+    Crack = 4,
+    DoorUp = 5,
+    DoorDown = 6,
+    DoorLeft = 7,
+    DoorRight = 8
 }
